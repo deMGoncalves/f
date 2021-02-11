f64845bbef7fed9ef1fba62570a85f8bd3a04eef
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2021-02-11 13:28:45 -0300

    feat(add): usando o rest, conseguimos trablhar mais de dois parametros

--- src/add/add.ts
+++ src/add/add.ts
@@ -2,8 +2,12 @@ declare type Add = {
   __add__(): number
 }
 
-function add (x: number | Add, y: number | Add): number {
-  return (x as number) + (y as number)
+function add (...args: Array<number|Add>): number {
+  return args.reduce<number>(reducer, 0)
+}
+
+function reducer (x: number, y: number | Add): number {
+  return x + (y as number)
 }
 
 export default add
