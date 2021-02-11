935b0b01049586db551e7836d379905e81e4130b
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2021-02-11 11:05:59 -0300

    feat(add): declaracao do tipo Add

--- src/add/add.ts
+++ src/add/add.ts
@@ -1,5 +1,9 @@
-function add (x: number, y: number): number {
-  return x + y
+declare type Add = {
+  __add__(): number
+}
+
+function add (x: number | Add, y: number | Add): number {
+  return (x as number) + (y as number)
 }
 
 export default add
